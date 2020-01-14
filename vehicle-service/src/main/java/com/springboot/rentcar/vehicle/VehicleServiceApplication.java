68d5bb96f76a1ff7a458928a77f2cdc6e2e9b140
Chaminda Sampath <sampatch@wm.ibm.com>
2020-01-14 21:53:55 +0800

    secured rest endpoint using oauth2

--- vehicle-service/src/main/java/com/springboot/rentcar/vehicle/VehicleServiceApplication.java
+++ vehicle-service/src/main/java/com/springboot/rentcar/vehicle/VehicleServiceApplication.java
@@ -3,9 +3,11 @@ package com.springboot.rentcar.vehicle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"com.springboot.rentcar.common.model"})
+@EnableResourceServer
 public class VehicleServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(VehicleServiceApplication.class, args);
